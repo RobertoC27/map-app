2a29af36a1338085bc875e8e87cade05d55d993c
RobertoC27 <chiroy_134@hotmail.com>
2019-03-22 14:17:24 -0600

    created mongoose models

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const  {ApolloServer}  = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to DB');
+
+    }).catch(err => { console.log(err); });
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
-server.listen().then(({url}) => {
+server.listen().then(({ url }) => {
     console.log(`Server listenting on ${url}`);
-    
+
 })
